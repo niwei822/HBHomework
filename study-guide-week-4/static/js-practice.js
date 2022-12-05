af0d7d8fb21379122060d5a12d744709352dc3da
niwei822 <cecily.li.822@gmail.com>
2022-12-04 23:04:15 -0800

    add js-practive

--- study-guide-week-4/static/js-practice.js
+++ study-guide-week-4/static/js-practice.js
@@ -5,19 +5,39 @@
 ********************* */
 
 // Your Code Here
-
+const button = document.querySelector('#popup-alert-button');
+button.addEventListener('click', () => alert('Hello!'));
 /** ***********************
 Practice Your Times Tables
 ************************ */
 
 // Your Code Here
+const multiplyForm = document.querySelector('#multiplying-form');
+multiplyForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
 
+  const multiplesOf = Number(document.querySelector('#mults-of').value);
+  const max = Number(document.querySelector('#max').value);
+  const multiples = [];
+  let nextMultiple = multiplesOf;
+
+  while (nextMultiple <= max) {
+    multiples.push(nextMultiple);
+    nextMultiple += multiplesOf;
+  }
+
+  console.log(multiples);
+});
 /** **************
 An Agreeable Form
 *************** */
 
 // Your Code Here
-
+document.querySelector('#agreeable-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const favoriteFood = document.querySelector('#favorite-food-input').value;
+  document.querySelector('#agreeable-text').textContent = `I like ${favoriteFood}, too!`;
+});
 /** ****************
 Five colored primes
 ***************** */
@@ -25,7 +45,35 @@ Five colored primes
 const PRIME_COLORS = ['orange', 'midnightblue', 'darkgoldenrod', 'green', 'purple'];
 
 // Your Code Here
+function isPrime(x) {
+  // is X prime?
 
+  for (let i = 2; i * i <= x; i++) {
+    if (x % i === 0) {
+      return false;
+    }
+  }
+  // We never found a divisor, so it's prime
+  return true;
+}
+
+function makePrimes() {
+  let num = 2; // find primes higher than 1
+  let numFound = 0;
+  document.querySelector('#prime-circle-area').innerHTML = '';
+  while (numFound < 5) {
+    if (isPrime(num)) {
+      const circleArea = document.querySelector('#prime-circle-area');
+      circleArea.insertAdjacentHTML(
+        'beforeend',
+        `<div class="prime-circle" style="background-color:${PRIME_COLORS[numFound]}">${num}</div>`,
+      );
+      numFound += 1;
+    }
+    num += 1;
+  }
+}
+document.querySelector('#make-prime-circles').addEventListener('click', makePrimes);
 /** *********
 Got Puppies?
 ********** */
@@ -42,3 +90,17 @@ function showPuppies(results) {
 }
 
 // Your Code Here
+document.querySelector('#puppy-form').addEventListener('submit', (event) => {
+  // prevent the form from triggering a page load
+  event.preventDefault();
+
+  // get the number of puppies selected from the input
+  const numPuppies = document.querySelector('#num-puppies').value;
+  // make the AJAX request
+  // GET requests cannot have a body so we use a query string
+  fetch(`/puppies.json?num-puppies=${numPuppies}`)
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      showPuppies(jsonResponse);
+    });
+});
